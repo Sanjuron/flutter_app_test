b5f7b2c6b1b9a944ff1763db2e54a7c2b5b2ed79
sanjuron <julien.naskret@gmail.com>
2020-11-11 19:30:56 +0100

    function

--- lib/App0201.dart
+++ lib/App0201.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
 
-main() => runApp(App0301());
+void main() => runApp(App0301());
 
 class App0301 extends StatelessWidget {
   Widget build(BuildContext context) {
-   return MaterialApp(
-     home: Scaffold(
-       appBar: AppBar(
-         title : Center(child: Text("Ma première image !")),
-       ),
-
-       body: Center(
-         child: Image.asset('porcorosso.jpg'),
-       ),
-     ),
-   );
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Text(
+            highlight("Regarde-moi"),
+          ),
+        ),
+      ),
+    );
   }
+}
+
+String highlight(String words) {
+  int numOfKazoos;
+  print (numOfKazoos);
+  print(numOfKazoos = 94);
+  return "*** " + words + " ***";
 }
