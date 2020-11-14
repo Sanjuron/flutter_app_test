2d36b718038eb59e915f1a5f088c66c922b80d15
sanjuron <julien.naskret@gmail.com>
2020-11-14 16:04:59 +0100

    commit

--- lib/App0201.dart
+++ lib/App0201.dart
@@ -3,22 +3,56 @@ import 'package:flutter/material.dart';
 void main() => runApp(App0301());
 
 class App0301 extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Text(
-            highlight("Regarde-moi"),
-          ),
-        ),
+      title : 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
       ),
+      home: MyHomePage(),
     );
   }
 }
 
-String highlight(String words) {
-  int numOfKazoos;
-  print (numOfKazoos);
-  print(numOfKazoos = 94);
-  return "*** " + words + " ***";
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State {
+  int _counter = 0;
+
+  void _incrementCounter(){
+    setState((){
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Listing"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Vous avez pressé le bouton autant de fois'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
 }
